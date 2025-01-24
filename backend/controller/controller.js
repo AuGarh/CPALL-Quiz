8b168f28c4f889eeed1bf2d42d3819e82e5ad3eb
nuthawut.l <au.garh1999@gmail.com>
2025-01-25 00:07:33 +0700

    feat(controller): Update product routes to use route parameters for ID

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -16,16 +16,16 @@ router.get('/api/v1/products', (req, res) => {
 
 router.put('/api/v1/products/:id', (req, res) => {
     const sql = "update product set `name` = ?, `price` = ?, `stock` = ? where `id` = ?";
-    const id = Number(req.query.id);
+    const id = Number(req.params.id);
     db.query(sql, [req.body.name, req.body.price, req.body.stock, id], (err, result) => {
         if (err) res.send(err);
         return res.json(result);
     });
 });
 
-router.delete('/api/v1/products', (req, res) => {
+router.delete('/api/v1/products/:id', (req, res) => {
     const sql = "delete from product where `id` = ?";
-    const id = Number(req.query.id);
+    const id = Number(req.params.id);
     db.query(sql, [id], (err, result) => {
         if (err) res.send(err);
         return res.json(result);
